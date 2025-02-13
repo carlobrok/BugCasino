e2e05dfed681a05ef2b773fba93c862646ab4bbd
Carlo Brokering <carlo.brokering@dlr.de>
2025-02-13 22:54:03 +0100

    Better feedback for the bet creation

--- src/lib/actions/bet.ts
+++ src/lib/actions/bet.ts
@@ -4,31 +4,48 @@ import { prisma } from "../prisma";
 import { getCurrentUser } from "../session";
 import type { User } from "@prisma/client";
 
+type BetResponse = {    
+    success: boolean;
+    error?: string;
+}
 
-export async function createBet(ticketId: number, amount: number, doneInTime: boolean) {
+export async function createBet(ticketId: number, amount: number, doneInTime: boolean) : Promise<BetResponse> {
     const user: User | null = await getCurrentUser();
 
     if (!user) {
-        return null;
+        return { success: false, error: "You are not authenticated" };
     }
 
     // check if the user has enough score
     if (user.score < amount) {
-        throw new Error("Not enough score");
+        return { success: false, error: "You don't have enough score" };
     }
 
-    await prisma.user.update({
-        where: { id: user.id },
-        data: { score: { decrement: amount } },
-    });
+    // backcheck if ticket is still open
+    const ticket = await prisma.ticket.findUnique({ where: { id: ticketId } });
+    if (ticket!.open === false) {
+        return { success: false, error: "Ticket is closed" };
+    }
 
-    // Return the result of updating the ticket(s)
-    return await prisma.bet.create({
-        data: {
-            amount,
-            ticketId,
-            userId: user.id,
-            doneInTime,
-        },
-    });
+    try {
+        // Decrement the user's score by the amount of the bet
+        await prisma.user.update({
+            where: { id: user.id },
+            data: { score: { decrement: amount } },
+        });
+        
+        // Return the result of updating the ticket(s)
+        await prisma.bet.create({
+            data: {
+                amount,
+                ticketId,
+                userId: user.id,
+                doneInTime,
+            },
+        });
+
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: "Some error occoured" };
+    }
 }
